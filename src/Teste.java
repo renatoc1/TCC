bf3d1346337bdc1158e3b901a4484c3ec29ba8a4
Renato Prata <renprata@hotmail.com>
2018-12-26 16:44:09 -0200

    Parte 4

--- src/Teste.java
+++ src/Teste.java
@@ -2,10 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import br.com.renato.models.Usuario;
 import br.com.renato.util.JPAUtil;
@@ -17,15 +17,14 @@ public class Teste {
 	public static void main(String[] args) throws IOException {
 		
 		try {
-				
-		String[] list = new String[TAMANHO_LISTA];
+						
 		Usuario usuario = null;
 		EntityManager em = new JPAUtil().getEntityManager();
 		String terceiroTermo = "";
 		String sextotermo = "";
 			
 		//Lê arquivo
-		FileInputStream fis = new FileInputStream("TesteFull.txt");
+		FileInputStream fis = new FileInputStream("Conversa1.txt");
 		InputStreamReader isr = new InputStreamReader(fis);
 		BufferedReader br = new BufferedReader(isr);
 		
@@ -35,7 +34,8 @@ public class Teste {
 		while (linha != null) {
 			
 			//Linha em branco
-			if (!linha.isEmpty()) {
+			
+			if (validLinha(linha)) {
 			
 				int i;
 				String dataFormato = "";
@@ -53,7 +53,8 @@ public class Teste {
 					terceiroTermo = linha.substring(2, 3);
 					sextotermo = linha.substring(5, 6);
 				} else {
-					terceiroTermo = linha.substring(2, 3);
+					//terceiroTermo = linha.substring(2, 3);
+					terceiroTermo = "";
 					sextotermo = "";
 				}
 				
@@ -104,19 +105,84 @@ public class Teste {
 		}	
 	}
 
+	private static boolean validLinha(String linha) {
+		String linhaStr = linha.trim();
+		if (linhaStr.isEmpty()) {
+			return false;
+		}
+		return true;
+	}
+
 	private static boolean formatoCorreto(String dtDia, String dtMes, String dtAno) {
 		return dtDia.length() == 2 && dtMes.length() == 2 && dtAno.length() == 4;
 	}
 
 	private static void salvarDados(Usuario usuario, String linha) {
-		String[] list;
+		
+		int i = 0, inicio = 0;
+		String frase = "", frasePessoa = "", fraseMensagem = "";
+		Boolean flag = false;
+		String[] list = new String[TAMANHO_LISTA];
 		list = linha.split(" - ");
+		frase = list[1].toString();
+		
 		usuario.setData(list[0]);
 		System.out.println("Data: " + list[0]);
-		usuario.setPessoa(list[1].split(": ")[0]);
-		System.out.println("Pessoa: " + list[1].split(": ")[0]);
-		usuario.setMensagem(list[1].split(": ")[1]);
-		System.out.println("Mensagem: " + list[1].split(": ")[1]);
+		
+		
+		
+		if (list[1].contains(": ")) {			
+			usuario.setPessoa(list[1].split(": ")[0]);
+			System.out.println("Pessoa: " + list[1].split(": ")[0]);			
+			usuario.setMensagem(list[1].split(": ")[1]);
+			System.out.println("Mensagem: " + list[1].split(": ")[1]);
+		} else {					
+			Pattern p = Pattern.compile("saiu");
+			Matcher m = p.matcher(frase);
+			while (m.find()) {
+				inicio = m.start();
+				System.out.println(m.start() + " " + m.group() +" "+ m.end());
+				flag = true;
+			}
+			if (flag) {			
+				//Percorre a String e busca a palavra "saiu"
+				if (inicio != 0) {
+					for (i = 2; i < inicio - 2; i++) {
+						frasePessoa += frase.charAt(i);
+						usuario.setPessoa(frasePessoa);
+						System.out.println(frasePessoa);
+						
+					}
+					for (i = inicio; i < frase.length(); i++) {
+						fraseMensagem += frase.charAt(i);
+						usuario.setMensagem(fraseMensagem);
+						System.out.println(fraseMensagem);
+					}
+				}			
+			} else {
+				Pattern p2 = Pattern.compile("entrou");
+				Matcher m2 = p2.matcher(frase);
+				while (m2.find()) {
+					inicio = m2.start();
+					System.out.println(m2.start() + " " + m2.group() +" "+ m2.end());
+					flag = false;
+				}
+				//Percorre a String e busca a palavra "entrou"
+				if (inicio != 0) {
+					for (i = 2; i < inicio - 1; i++) {
+						frasePessoa += frase.charAt(i);
+						usuario.setPessoa(frasePessoa);
+						System.out.println(frasePessoa);
+						
+					}
+					for (i = inicio; i < frase.length(); i++) {
+						fraseMensagem += frase.charAt(i);
+						usuario.setMensagem(fraseMensagem);
+						System.out.println(fraseMensagem);
+					}
+				}			
+			}
+		}		
 	}
-
+		
 }
